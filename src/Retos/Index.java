274cdb623973a24b311069f42ab5fe14c596486a
FJRentero <F.J.Rentero@gmail.com>
2024-05-12 22:50:19 +0200

    añadida la clase Index solo comentada y ignorados los demas archivos excepto .gitignore y .gitignore - .settings

---
+++ src/Retos/Index.java
@@ -0,0 +1,10 @@
+package Retos;
+//clase principal desde la que se ejecutaran todos los ejercicios
+public class Index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
